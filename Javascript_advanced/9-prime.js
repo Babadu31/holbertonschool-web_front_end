8358e2805d4533792523488cf5d21f42a50cf5a4
Babadu31 <105010364+Babadu31@users.noreply.github.com>
2023-05-11 18:00:04 +0200

    11/05/23-18:00-9.1

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,23 @@
+function countPrimeNumbers() {
+    var count = 0;
+    for (var i = 2; i <= 100; i++) {
+      var isPrime = true;
+      for (var j = 2; j < i; j++) {
+        if (i % j === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) {
+        count++;
+      }
+    }
+    return count;
+  }
+  
+  var start = performance.now();
+  var primeCount = countPrimeNumbers();
+  var end = performance.now();
+  var executionTime = end - start;
+  console.log('Execution time of printing countPrimeNumbers was ' + executionTime + ' milliseconds.');
+  
